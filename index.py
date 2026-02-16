2c08d420cc0817ccab755ac219eb9b5f1bcfe330
Raiyok <ronitteli1@gmail.com>
2026-02-16 09:07:05 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,62 +1,20 @@
 import time
 import picamera
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 
-# Motor pins
-m11 = 17
-m12 = 27
+m11=17
+m12=27
+button=19
 
-# Button pin
-button = 19
+gpio.setwarnings(False)
+gpio.setmode(gpio.BCM)
+gpio.setup(m11,gpio.OUT)
+gpio.setup(m12,gpio.OUT)
+gpio.setup(button,gpio.IN)
 
-# GPIO setup
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(m11, GPIO.OUT)
-GPIO.setup(m12, GPIO.OUT)
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-GPIO.output(m11, GPIO.LOW)
-GPIO.output(m12, GPIO.LOW)
-
-# Create camera object
-camera = picamera.PiCamera()
+gpio.output(m11,0)
+gpio.output(m12,0)
 
 def capture_image():
-    print("Capturing image...")
-    camera.resolution = (1280, 720)
+    camera.resolution=(1280,720)
     camera.start_preview()
-    time.sleep(2) # Allow camera to warm up
-    camera.capture("/home/pi/image.jpg")
-    camera.stop_preview()
-    print("Image saved!")
-
-try:
-    print("Press button to capture image & run motor")
-
-    while True:
-        if GPIO.input(button) == GPIO.LOW: # Button pressed
-            print("Button Pressed!")
-
-            # Motor ON
-            GPIO.output(m11, GPIO.HIGH)
-            GPIO.output(m12, GPIO.LOW)
-
-            capture_image()
-
-            time.sleep(2)
-
-            # Motor OFF
-            GPIO.output(m11, GPIO.LOW)
-            GPIO.output(m12, GPIO.LOW)
-
-            time.sleep(1)
-
-except KeyboardInterrupt:
-    print("Stopped by user")
-
-finally:
-    GPIO.cleanup()
-    camera.close()
-    print("GPIO Cleaned")
